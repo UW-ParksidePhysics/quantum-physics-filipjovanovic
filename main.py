21b99db54d6016e3254aef3e8a61c2181a7f5ac5
jovan002 <jovan002@rangers.uwp.edu>
2021-03-06 22:08:13 -0600

    Modified to work for Quantum Problem 2.56

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ def main():
     potential_width = 1.0
     positions = np.linspace(-3*potential_width, 3 * potential_width, num=10000)
 
-    n = 0   # Initial Energy State
+    n = 1   # Initial Energy State
     n_max = 3  # Final Energy State
 
-    while n <= n_max - 1:
+    while n <= n_max:
         k = (2*n) + 1
-        wag_range = np.linspace(0.95, 1.05, num=5)
+        wag_range = np.linspace(0.85, 1.15, num=5)
         adjustable_coefficients = k * wag_range
         initial_conditions = (1., 0.)  # psi(0), psi'(0)
         wag_the_dog('harmonic oscillator', adjustable_coefficients, potential_width, positions, initial_conditions)
